dc908d6c9354e3956f6f9e7d8568b4c888f90fea
Preston <preston159@gmail.com>
2019-11-07 23:20:47 -0700

    got liri fully functioning.  Still need to complete ReadMe

--- liri.js
+++ liri.js
@@ -4,36 +4,61 @@ var keys = require("./keys.js");
 
 var axios = require("axios");
 
+var Spotify = require("node-spotify-api");
 
+var fs = require("fs");
+
+const cTable = require("console.table");
+
+var spotify = new Spotify(keys.spotify);
  
 console.log(keys.spotify.id);
 
 var userCommand = process.argv[2];
 
-var userInput = process.argv[3];
+var userInput = process.argv.slice(3).join(" ");
 
-var runUserCommand = function () {
-    if (userCommand === "concert-this") {
-        concertResults();
-    } else if (userCommand === "spotify-this-song") {
-        songResults();
-    } else if (userCommand === "movie-this") {
-        movieResults();
-    } else if (userCommand === "do-what-it-says") {
-        followCommand();
-    } else {
-        console.log("That is not a command");
+function lineBreak(){
+    console.log("--------------------------------------");
+}
+function seperator(){
+    console.log("\n")
+}
+
+var runUserCommand = function (command, input) {
+
+
+    switch (command) {
+        case "concert-this":
+            concertResults(input);
+        break;
+        case "spotify-this-song":
+            songResults(input);
+        break;
+        case "movie-this":
+            movieResults(input);
+        break;
+        case "do-what-it-says":
+            followCommand(input);
+            break;
+        default: 
+            console.log("that is not a command");
+            break;
     }
 }
 
-var concertResults = function () {
-    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+var concertResults = function (input) {
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
         .then(function (response) {
             for (i = 0; i < response.data.length; i++) {
-                console.log(response.data[i].venue.name);
-                console.log(response.data[i].venue.city);
-                console.log(response.data[i].venue.country);
-                console.log(response.data[i].datetime);
+                console.table([
+                    {
+                        Venue: response.data[i].venue.name,
+                        City: response.data[i].venue.city,
+                        Country: response.data[i].venue.country,
+                        Date_and_Time: response.data[i].datetime
+                    }
+                ])
                 
             }
         })
@@ -42,4 +67,70 @@ var concertResults = function () {
         })
 }
 
-runUserCommand();
+
+
+function movieResults(input) {
+    
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + input)
+    .then(function(response) {
+        
+            seperator();
+            console.log("Title: " + response.data.Title);
+            lineBreak();
+            console.log("Year Released: " + response.data.Year);
+            lineBreak();
+            console.log("Imdb: " + response.data.Ratings[0].Value);
+            lineBreak();
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value)
+            lineBreak();
+            console.log("Metacritic: " + response.data.Ratings[2].Value)
+            lineBreak();
+            console.log("Country: " + response.data.Country);
+            lineBreak();
+            console.log("Plot Summary: " + response.data.Plot);
+            lineBreak();
+            console.log("Leading Actors: " + response.data.Actors);
+        
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
+
+
+
+var songResults = function(input) {
+    spotify.search({
+        type: "track",
+        query: input,
+        
+        
+    }, function(err, data) {
+        if (err) {
+            return console.log("Error occured: " + err);
+        }else{
+            var info = data.tracks.items[0];
+           console.table([{
+            artist: info.artists[0].name, 
+            Song: info.name, 
+            URL: info.external_urls.spotify, 
+            album: info.album.name
+            }]);
+
+            
+        }
+    });
+}
+
+
+function followCommand(){
+    fs.readFile(__dirname + "/random.txt", function(err, data) {
+        if (err) throw err;
+        console.log(data.toString());
+        var command = data.toString().split(",");
+        runUserCommand(command[0], command[1]);
+    })
+}
+
+
+runUserCommand(userCommand, userInput);
